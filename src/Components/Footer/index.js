3f7dcfb41edf03f5a8198bcba3eeeaaae344ebe3
franncesco <francesco.coriolano@gmail.com>
2022-05-05 09:30:03 +0200

    edit footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,6 +7,32 @@ const Footer = () => {
         Made with ❤️ by{" "}
         <a href="https://www.linkedin.com/in/francesco-coriolano/">Francesco</a>
       </h4>
+      <div className="social-buttons-container">
+        <a className="social-button facebook" href="https://www.facebook.com/">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a className="social-button twitter" href="https://www.twitter.com/">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a
+          className="social-button linkedin"
+          href="https://www.linkedin.com/in/francesco-coriolano/"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a
+          className="social-button github"
+          href="https://github.com/francescocori"
+        >
+          <i className="fab fa-github"></i>
+        </a>
+        <a
+          className="social-button instagram"
+          href="https://www.instagram.com/"
+        >
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
     </div>
   );
 };
