2820d60d8514c3bf457ef131423a8735921f7503
franncesco <francesco.coriolano@gmail.com>
2022-05-10 15:29:04 +0200

    small change on style

--- src/Components/FinalScore/index.js
+++ src/Components/FinalScore/index.js
@@ -4,7 +4,11 @@ const FinalScore = ({ startGame, score }) => {
   return (
     <div>
       <div className="modal-overlay">
-        <h4>{`Your score is ${score}  /10`}</h4>
+        <h4>
+          Your score is <br />
+          {` ${score}  /10`}
+        </h4>
+        {/* <h4>{` ${score}  /10`}</h4> */}
         <button className="start-button" onClick={startGame}>
           {" "}
           Play Again{" "}
