f551b6d5737261688f99d3d3a411129d165013bb
franncesco <francesco.coriolano@gmail.com>
2022-05-10 14:50:18 +0200

    edit answers style

--- src/Components/Questions/index.js
+++ src/Components/Questions/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./style.css";
 const Questions = ({ currentQuestion }) => {
   return (
-    <div>
+    <div className="question-container">
       {/* code below is not best pratcice but for now seems the only way to remove unwanted caracter */}
-      <h2 dangerouslySetInnerHTML={{ __html: currentQuestion }} />
+      <h2
+        className="question"
+        dangerouslySetInnerHTML={{ __html: currentQuestion }}
+      />
     </div>
   );
 };
