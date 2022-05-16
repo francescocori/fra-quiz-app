42900bf210263c665c7f58d26418f077a5cbc55e
franncesco <francesco.coriolano@gmail.com>
2022-05-16 08:25:55 +0200

    added nanoid

--- src/Components/Answers/index.js
+++ src/Components/Answers/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./style.css";
-
+import { nanoid } from "nanoid";
 const Answers = ({ handleClickedAnswer, correctAnswers, wrongAnswers }) => {
   let mixedAnswer = [...wrongAnswers, correctAnswers];
   mixedAnswer.sort((a, b) => 0.5 - Math.random());
   return (
     <div className="answer-container">
-      {mixedAnswer.map((answer, index) => {
+      {mixedAnswer.map((answer) => {
         return (
           <button
             className="answer-cell"
             value={answer}
-            key={index}
-            id={index}
+            key={nanoid()}
             onClick={(e) => handleClickedAnswer(e)}
+            //code below is not best pratcice but for now seems the only way to remove unwanted caracter
             dangerouslySetInnerHTML={{ __html: answer }}
           />
         );
